0750bc2eb1b3c78b96c8c0946cbd8082edd48fe2
Evgeniy Gutman <Snipe76@gmail.com>
2025-02-12 22:17:23 +0200

    Tweaks for phones

--- script/burger.js
+++ script/burger.js
@@ -1,6 +1,6 @@
 function toggleMenu() {
     var headerNavLinks = document.getElementById("nav-links");
-    if (headerNavLinks.style.display === "flex" && window.innerWidth < 1200) {
+    if (headerNavLinks.style.display === "flex" && window.innerWidth < 1440) {
         headerNavLinks.style.display = "none";
     } else {
         headerNavLinks.style.display = "flex";
