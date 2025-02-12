8128ae8d4ff598e86bed39e92c355da48a5e5016
Evgeniy Gutman <Snipe76@gmail.com>
2025-02-12 22:14:28 +0200

    Fixed burger bugs

--- script/burger.js
+++ script/burger.js
@@ -1,6 +1,6 @@
 function toggleMenu() {
     var headerNavLinks = document.getElementById("nav-links");
-    if (headerNavLinks.style.display === "flex") {
+    if (headerNavLinks.style.display === "flex" && window.innerWidth < 1200) {
         headerNavLinks.style.display = "none";
     } else {
         headerNavLinks.style.display = "flex";
